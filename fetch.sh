3f878a26622b7fc7e0decd57e65ce62a0070ad52
Noshiro Nanai <epm.maya@gmail.com>
2020-12-11 11:42:10 +0900

    Bump up openssl 1.1.1i, curl 7.74.0

--- fetch.sh
+++ fetch.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-git clone --depth 1 --single-branch -b OpenSSL_1_1_1h https://github.com/openssl/openssl.git
+git clone --depth 1 --single-branch -b OpenSSL_1_1_1i https://github.com/openssl/openssl.git
 
 pushd openssl
 git submodule update --init --recursive --recommend-shallow --depth 1
 popd
 
-git clone --depth 1 --single-branch -b curl-7_73_0 https://github.com/curl/curl.git
+git clone --depth 1 --single-branch -b curl-7_74_0 https://github.com/curl/curl.git
 
